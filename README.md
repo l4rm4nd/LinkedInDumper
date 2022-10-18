3960612c4fa259c161a576dfa3493ced658b3974
L4RM4ND <21357789+l4rm4nd@users.noreply.github.com>
2022-10-18 23:43:40 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# LinkedInDumper
+Python 3 script to dump company employees from LinkedIn Voyager API.
+
+The results contain firstname, lastname, position (title), location and a user's profile link. Only 2 API calls are required to retrieve all employees if the company does not have more than 10 employees. Otherwise, we have to paginate through the API results. The API returns a maximum of 1000 employees.
+
+## Limitations
+
+LinkedIn will allow only the first 1,000 search results to be returned when harvesting contact information. However, the same results are not returned each time a series of searches are applied. Run the script multiple times to capture more contacts. Moreover, the amount of allowed profile searches is limited. You may need a LinkedIn premium account when you reached the maximum allowed queries.
+
+Furthermore, not all employee profiles are public. The results vary depending on your used LinkedIn account and whether you are befriended with some employees of the company to crawl or not. Therefore, it is sometimes not possible to retrieve the firstname, lastname and profile url of some employee accounts. The script will not display such profiles, as they contain default values such as "LinkedIn" as firstname and "Member" in the lastname. If you want to include such private profiles, please use the CLI flag ``--include-private-profiles``. Although some accounts may be private, we can obtain the position (title) as well as the location of such accounts. Only firstname, lastname and profile URL are hidden for private LinkedIn accounts.
+
+Finally, LinkedIn users are free to name their profile. An account name can therefore consist of various things such as saluations, abbreviations, emojis, middle names etc. I tried my best to remove some nonsense. However, this is not a complete solution to the general problem.
+ 
+## How-To
+1. Sign into www.linkedin.com and retrieve your ``li_at`` session cookie value e.g. via developer tools. I recommend not using your real LinkedIn account.
+2. Specify the cookie value either persistently in the python script's variable ``li_at`` or temporarily via the CLI flag ``--session-cookie``
+3. Browse your company on LinkedIn and note the url. Must be something like https://www.linkedin.com/company/apple
+4. Install requirements via ``pip install -r requirements.txt``
+5. Run the Python script and enjoy results
+
+## Usage
+````
+usage: linkedindumper.py [-h] --url <linkedin-url> [--session-cookie <cookie>] [--quiet] [--include-private-profiles]
+
+options:
+  -h, --help                    show this help message and exit
+  --url <linkedin-url>          A LinkedIn company url - https://www.linkedin.com/company/<company>
+  --session-cookie <cookie>     LinkedIn 'li_at' session cookie
+  --quiet                       Show employee results only
+  --include-private-profiles    Show private accounts too
+````
+
+## Examples
+
+Dumping Apple employees from LinkedIn API into outfile using `--quiet` mode:
+````
+python3 linkedindumper.py --url https://www.linkedin.com/company/apple --quiet > apple_employees.out
+````
+
+## Results
+
+The script will return employee data as semi-colon separated values (like CSV):
+
+````
+ ██▓     ██▓ ███▄    █  ██ ▄█▀▓█████ ▓█████▄  ██▓ ███▄    █ ▓█████▄  █    ██  ███▄ ▄███▓ ██▓███  ▓█████  ██▀███  
+▓██▒    ▓██▒ ██ ▀█   █  ██▄█▒ ▓█   ▀ ▒██▀ ██▌▓██▒ ██ ▀█   █ ▒██▀ ██▌ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒
+▒██░    ▒██▒▓██  ▀█ ██▒▓███▄░ ▒███   ░██   █▌▒██▒▓██  ▀█ ██▒░██   █▌▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▒███   ▓██ ░▄█ ▒
+▒██░    ░██░▓██▒  ▐▌██▒▓██ █▄ ▒▓█  ▄ ░▓█▄   ▌░██░▓██▒  ▐▌██▒░▓█▄   ▌▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄  
+░██████▒░██░▒██░   ▓██░▒██▒ █▄░▒████▒░▒████▓ ░██░▒██░   ▓██░░▒████▓ ▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░░▒████▒░██▓ ▒██▒
+░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ▒ ▒▒ ▓▒░░ ▒░ ░ ▒▒▓  ▒ ░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░░ ░▒ ▒░ ░ ░  ░ ░ ▒  ▒  ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒ ░░▒░ ░ ░ ░  ░      ░░▒ ░      ░ ░  ░  ░▒ ░ ▒░
+  ░ ░    ▒ ░   ░   ░ ░ ░ ░░ ░    ░    ░ ░  ░  ▒ ░   ░   ░ ░  ░ ░  ░  ░░░ ░ ░ ░      ░   ░░          ░     ░░   ░ 
+    ░  ░ ░           ░ ░  ░      ░  ░   ░     ░           ░    ░       ░            ░               ░  ░   ░     
+                                      ░                      ░                                         ░ by LRVT      
+
+[i] Company Name: apple
+[i] Company X-ID: 162479
+[i] LN Employees: 1000 employees found
+[i] Dumping Date: 17/10/2022 13:55:06
+
+Firstname;Lastname;Position;Gender;Location;Profile
+Katrin;Honauer;Software Engineer at Apple;N/A;Heidelberg;https://www.linkedin.com/in/katrin-honauer
+Raymond;Chen;Recruiting at Apple ;N/A;Austin, Texas Metropolitan Area;https://www.linkedin.com/in/raytherecruiter
+Denny;Brem;System Engineer bei Apple;N/A;Greater Munich Metropolitan Area;https://www.linkedin.com/in/denny-brem
+
+[i] Successfully crawled 3 unique apple employee(s). Hurray ^_-
+````
---
+++ linkedindumper.py
@@ -0,0 +1,216 @@
+import requests
+import json
+import re
+import argparse
+import sys
+import time
+import unidecode
+from datetime import datetime
+
+# mandatory authenticated session cookie
+li_at = "<SESSION-COOKIE>"
+
+parser = argparse.ArgumentParser("linkedindumper.py")
+parser.add_argument("--url", metavar='<linkedin-url>', help="A LinkedIn company url - https://www.linkedin.com/company/<company>", type=str, required=True)
+parser.add_argument("--session-cookie", metavar='<cookie>', help="LinkedIn 'li_at' session cookie", type=str, required=False,)
+parser.add_argument("--quiet", help="Show employee results only", required=False, action='store_true')
+parser.add_argument("--include-private-profiles", help="Show private accounts too", required=False, action='store_true')
+
+args = parser.parse_args()
+url = args.url
+
+# optional CSRF token, not needed for GET requests but still defined to be sure
+JSESSIONID = "ajax:1337133713371337"
+
+# overwrite varibales if set via CLI
+if (args.session_cookie):
+	li_at = args.session_cookie
+
+headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', 'Content-type': 'application/json', 'Csrf-Token': JSESSIONID}
+cookies_dict = {"li_at": li_at, "JSESSIONID": JSESSIONID} 
+
+if (url.startswith('https://www.linkedin.com/company/')):
+	try:
+		# extract company slug from given LinkedIn URL
+		before_keyword, keyword, after_keyword = url.partition('company/')
+		company = after_keyword
+
+		api1 = "https://www.linkedin.com/voyager/api/voyagerOrganizationDashCompanies?decorationId=com.linkedin.voyager.dash.deco.organization.MiniCompany-10&q=universalName&universalName=" + str(company)
+		# request to query a company's urn ID
+		r = requests.get(api1, headers=headers, cookies=cookies_dict)
+		response1 = r.json()
+
+		# retrieve company urn from the api; e.g. "urn:li:fsd_company:42848399"
+		companyID = response1["elements"][0]["entityUrn"].split(":")[-1]
+
+		# we'll use paging count 10 since it seems to work best
+		paging_count = 10
+
+		api2 = "https://www.linkedin.com/voyager/api/search/dash/clusters?decorationId=com.linkedin.voyager.dash.deco.search.SearchClusterCollection-165&origin=COMPANY_PAGE_CANNED_SEARCH&q=all&query=(flagshipSearchIntent:SEARCH_SRP,queryParameters:(currentCompany:List(" + str(companyID) + "),resultType:List(PEOPLE)),includeFiltersInResponse:false)&count=" + str(paging_count)+ "&start=0"
+		# retrieve employee information from the api based on previously obtained company id
+		r2 = requests.get(api2, headers=headers, cookies=cookies_dict)
+		response2 = r2.json()
+
+		paging_total = response2["paging"]["total"]
+		required_pagings = -(-paging_total // paging_count)
+
+		if not args.quiet:
+
+			print("""\
+
+ ██▓     ██▓ ███▄    █  ██ ▄█▀▓█████ ▓█████▄  ██▓ ███▄    █ ▓█████▄  █    ██  ███▄ ▄███▓ ██▓███  ▓█████  ██▀███  
+▓██▒    ▓██▒ ██ ▀█   █  ██▄█▒ ▓█   ▀ ▒██▀ ██▌▓██▒ ██ ▀█   █ ▒██▀ ██▌ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒
+▒██░    ▒██▒▓██  ▀█ ██▒▓███▄░ ▒███   ░██   █▌▒██▒▓██  ▀█ ██▒░██   █▌▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▒███   ▓██ ░▄█ ▒
+▒██░    ░██░▓██▒  ▐▌██▒▓██ █▄ ▒▓█  ▄ ░▓█▄   ▌░██░▓██▒  ▐▌██▒░▓█▄   ▌▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▒▓█  ▄ ▒██▀▀█▄  
+░██████▒░██░▒██░   ▓██░▒██▒ █▄░▒████▒░▒████▓ ░██░▒██░   ▓██░░▒████▓ ▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░░▒████▒░██▓ ▒██▒
+░ ▒░▓  ░░▓  ░ ▒░   ▒ ▒ ▒ ▒▒ ▓▒░░ ▒░ ░ ▒▒▓  ▒ ░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░
+░ ░ ▒  ░ ▒ ░░ ░░   ░ ▒░░ ░▒ ▒░ ░ ░  ░ ░ ▒  ▒  ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒ ░░▒░ ░ ░ ░  ░      ░░▒ ░      ░ ░  ░  ░▒ ░ ▒░
+  ░ ░    ▒ ░   ░   ░ ░ ░ ░░ ░    ░    ░ ░  ░  ▒ ░   ░   ░ ░  ░ ░  ░  ░░░ ░ ░ ░      ░   ░░          ░     ░░   ░ 
+    ░  ░ ░           ░ ░  ░      ░  ░   ░     ░           ░    ░       ░            ░               ░  ░   ░     
+                                      ░                      ░                                         ░ by LRVT      
+			""")
+
+			print("[i] Company Name: " + company)
+			print("[i] Company X-ID: " + companyID)
+			print("[i] LN Employees: " + str(paging_total) + " employees found")
+			print("[i] Dumping Date: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+			print()
+
+		dump_count = 0
+
+		def remove_emojis(data):
+			emoj = re.compile("["
+						u"\U0001F600-\U0001F64F"  # emoticons
+						u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+						u"\U0001F680-\U0001F6FF"  # transport & map symbols
+						u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+						u"\U00002500-\U00002BEF"  # chinese char
+						u"\U00002702-\U000027B0"
+						u"\U00002702-\U000027B0"
+						u"\U000024C2-\U0001F251"
+						u"\U0001f926-\U0001f937"
+						u"\U00010000-\U0010ffff"
+						u"\u2640-\u2642" 
+						u"\u2600-\u2B55"
+						u"\u200d"
+						u"\u23cf"
+						u"\u23e9"
+						u"\u231a"
+						u"\ufe0f"  # dingbats
+						u"\u3030"
+													"]+", re.UNICODE)
+			# remove trailing space, emojis and diacritics
+			return unidecode.unidecode(re.sub(emoj, '', data).rstrip())
+
+		def progressbar(it, prefix="", size=60, out=sys.stdout): # Python3.3+
+				count = len(it)
+				def show(j):
+						x = int(size*j/count)
+						print("{}[{}{}] {}/{}".format(prefix, "#"*x, "."*(size-x), j, count),
+										end='\r', file=out, flush=True)
+				show(0)
+				for i, item in enumerate(it):
+						yield item
+						show(i+1)
+				print("\n", flush=True, file=out)
+
+		employee_dict = []
+		# paginate API results
+		for page in progressbar(range(required_pagings), "Progress: ", 40):
+			api2 = "https://www.linkedin.com/voyager/api/search/dash/clusters?decorationId=com.linkedin.voyager.dash.deco.search.SearchClusterCollection-165&origin=COMPANY_PAGE_CANNED_SEARCH&q=all&query=(flagshipSearchIntent:SEARCH_SRP,queryParameters:(currentCompany:List(" + str(companyID) + "),resultType:List(PEOPLE)),includeFiltersInResponse:false)&count=" + str(paging_count)+ "&start=" + str(page*10)
+			# retrieve employee information from the api based on previously obtained company id
+			r2 = requests.get(api2, headers=headers, cookies=cookies_dict)
+			response2 = r2.json()
+			try:
+				test = response2["elements"][0]["results"][0]["title"]["text"]
+				results = response2["elements"][0]["results"]
+			except:
+				pass
+
+			try:
+				test = response2["elements"][1]["results"][0]["title"]["text"]
+				results = response2["elements"][1]["results"]
+			except:
+				pass
+
+			try:
+				test = response2["elements"][2]["results"][0]["title"]["text"]
+				results = response2["elements"][2]["results"]
+			except:
+				pass
+
+			for employee in results:
+				# get a user's full account name and remove some known abbreviations and salutations
+				account_name = remove_emojis(employee["title"]["text"]).split(" ")
+				badwords = ['Prof.', 'Dr.', 'M.A.', ',', 'LL.M.']
+				for word in list(account_name):
+					if word in badwords:
+						account_name.remove(word)
+
+				# if the account name consists of 2 strings, assume firstname and lastname
+				if len(account_name) == 2:
+					firstname = account_name[0].replace(',', '').replace(';', ',')
+					lastname = account_name[1].replace(',', '').replace(';', ',')
+				# otherwise it is some unknown format with saluation, or middle name or random abbreviations
+				else:
+					# combine everything up to last string into firstname
+					firstname = ' '.join(map(str,account_name[0:(len(account_name)-1)])).replace(',', '').replace(';', ',')
+					# use the last string as lastname
+					lastname = account_name[-1].replace(',', '').replace(';', ',')
+				
+				try:
+					position = remove_emojis(employee["primarySubtitle"]["text"]).replace(';',',')
+				except:
+					position = "N/A"
+				gender = "N/A"
+				
+				# an account's location is sometimes unaccessible
+				try:
+					location = employee["secondarySubtitle"]["text"]
+				except:
+					location = "N/A"
+				
+				profile_link = employee["navigationUrl"].split("?")[0]
+
+				if args.include_private_profiles:
+					employee_dict.append({"firstname":remove_emojis(firstname), "lastname":remove_emojis(lastname), "position":remove_emojis(position), "gender":gender, "location":location, "profile_link":profile_link})
+					dump_count += 1
+				else:
+					if (firstname != "LinkedIn" and lastname != "Member"):
+						employee_dict.append({"firstname":remove_emojis(firstname), "lastname":remove_emojis(lastname), "position":remove_emojis(position), "gender":gender, "location":location, "profile_link":profile_link})
+						dump_count += 1
+
+		# remove duplicates from list in dict
+		l = employee_dict
+		seen = set()
+		new_l = []
+		for d in l:
+			t = tuple(sorted(d.items()))
+			if t not in seen:
+				seen.add(t)
+				new_l.append(d)
+
+			# this is the new dict with unique employees
+			employee_dict = new_l
+
+		legende = "Firstname;Lastname;Position;Gender;Location;Profile"
+		print(legende)
+
+		# dump all crawled employees
+		for person in employee_dict:
+			print(";".join(person.values()))
+
+		if not args.quiet:
+			print()
+			print("[i] Successfully crawled " + str(len(employee_dict)) + " unique " + str(company) + " employee(s). Hurray ^_-")
+	
+	except Exception as e:
+		# likely authorization error due to incorrect 'login' cookie
+		# otherwise the script is broken or the api has been changed
+		print("[!] Authenticated session cookie required.")
+		print("    > Set variable permanently in script or use the '--session-cookie' CLI flag!")
+		print("[debug] " + str(e))
+else:
+	print()
+	print("[!] Invalid URL provided.")
+	print("[i] Example URL: 'https://www.linkedin.com/company/apple'")
