74146730c6003db36c1110900849d7fef9fab9aa
L4RM4ND <21357789+l4rm4nd@users.noreply.github.com>
2024-07-21 18:43:53 +0200

    fix(code): refactor the py script
    Introduce functions and display ascii code before API HTTP calls.

--- linkedindumper.py
+++ linkedindumper.py
@@ -10,11 +10,12 @@ import unidecode
 from datetime import datetime
 import urllib.parse
 import textwrap
+import threading
 
-# you may store your session cookie here persistently
+# You may store your session cookie here persistently
 li_at = "YOUR-COOKIE-VALUE"
 
-# converting german umlauts
+# Converting German umlauts
 special_char_map = {ord('ä'):'ae', ord('ü'):'ue', ord('ö'):'oe', ord('ß'):'ss'}
 
 format_examples = '''
@@ -27,7 +28,7 @@ format_examples = '''
 
 parser = argparse.ArgumentParser("linkedindumper.py", formatter_class=RawTextHelpFormatter)
 parser.add_argument("--url", metavar='<linkedin-url>', help="A LinkedIn company url - https://www.linkedin.com/company/<company>", type=str, required=True)
-parser.add_argument("--cookie", metavar='<cookie>', help="LinkedIn 'li_at' session cookie", type=str, required=False,)
+parser.add_argument("--cookie", metavar='<cookie>', help="LinkedIn 'li_at' session cookie", type=str, required=False)
 parser.add_argument("--quiet", help="Show employee results only", required=False, action='store_true')
 parser.add_argument("--include-private-profiles", help="Show private accounts too", required=False, action='store_true')
 parser.add_argument("--jitter", help="Add a random jitter to HTTP requests", required=False, action='store_true')
@@ -36,14 +37,14 @@ parser.add_argument("--email-format", help="Python string format for emails; for
 args = parser.parse_args()
 url = args.url
 
-# optional CSRF token, not needed for GET requests but still defined to be sure
+# Optional CSRF token, not needed for GET requests but still defined to be sure
 JSESSIONID = "ajax:5739908118104050450"
 
-# overwrite varibales if set via CLI
-if (args.cookie):
+# Overwrite variables if set via CLI
+if args.cookie:
 	li_at = args.cookie
 
-if (args.email_format):
+if args.email_format:
 	mailformat = args.email_format
 else:
 	mailformat = False
@@ -51,37 +52,8 @@ else:
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0', 'Content-type': 'application/json', 'Csrf-Token': JSESSIONID}
 cookies_dict = {"li_at": li_at, "JSESSIONID": JSESSIONID}
 
-if (url.startswith('https://www.linkedin.com/company/')):
-	try:
-		# extract company slug from given LinkedIn URL
-		before_keyword, keyword, after_keyword = url.partition('company/')
-		company = after_keyword.split('/')[0]
-
-		# url encode potential special chars that will brick the api lookup
-		company = urllib.parse.quote(company)
-
-		api1 = "https://www.linkedin.com/voyager/api/voyagerOrganizationDashCompanies?decorationId=com.linkedin.voyager.dash.deco.organization.MiniCompany-10&q=universalName&universalName=" + str(company)
-		# request to query a company's urn ID
-		r = requests.get(api1, headers=headers, cookies=cookies_dict)
-		response1 = r.json()
-
-		# retrieve company urn from the api; e.g. "urn:li:fsd_company:42848399"
-		companyID = response1["elements"][0]["entityUrn"].split(":")[-1]
-
-		# we'll use paging count 10 since it seems to work best
-		paging_count = 10
-
-		api2 = "https://www.linkedin.com/voyager/api/search/dash/clusters?decorationId=com.linkedin.voyager.dash.deco.search.SearchClusterCollection-165&origin=COMPANY_PAGE_CANNED_SEARCH&q=all&query=(flagshipSearchIntent:SEARCH_SRP,queryParameters:(currentCompany:List(" + str(companyID) + "),resultType:List(PEOPLE)),includeFiltersInResponse:false)&count=" + str(paging_count)+ "&start=0"
-		# retrieve employee information from the api based on previously obtained company id
-		r2 = requests.get(api2, headers=headers, cookies=cookies_dict)
-		response2 = r2.json()
-
-		paging_total = response2["paging"]["total"]
-		required_pagings = -(-paging_total // paging_count)
-
-		if not args.quiet:
-
-			print("""\
+def print_logo():
+	print("""\
 
  ██▓     ██▓ ███▄    █  ██ ▄█▀▓█████ ▓█████▄  ██▓ ███▄    █ ▓█████▄  █    ██  ███▄ ▄███▓ ██▓███  ▓█████  ██▀███  
 ▓██▒    ▓██▒ ██ ▀█   █  ██▄█▒ ▓█   ▀ ▒██▀ ██▌▓██▒ ██ ▀█   █ ▒██▀ ██▌ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒▓█   ▀ ▓██ ▒ ██▒
@@ -93,181 +65,208 @@ if (url.startswith('https://www.linkedin.com/company/')):
   ░ ░    ▒ ░   ░   ░ ░ ░ ░░ ░    ░    ░ ░  ░  ▒ ░   ░   ░ ░  ░ ░  ░  ░░░ ░ ░ ░      ░   ░░          ░     ░░   ░ 
     ░  ░ ░           ░ ░  ░      ░  ░   ░     ░           ░    ░       ░            ░               ░  ░   ░     
                                       ░                      ░                                         ░ by LRVT      
-			""")
+	""")
 
-			print("[i] Company Name: " + company)
-			print("[i] Company X-ID: " + companyID)
-			print("[i] LN Employees: " + str(paging_total) + " employees found")
-			print("[i] Dumping Date: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-			if mailformat:
-				print("[i] Email Format: " + mailformat)
-			print()
+def show_loading_message(stop_event):
+	loading_message = "Please be patient"
+	while not stop_event.is_set():
+		for _ in range(3):
+			if stop_event.is_set():
+				break
+			for dot in range(4):
+				sys.stdout.write("\r" + loading_message + "." * dot + " " * (3 - dot))
+				sys.stdout.flush()
+				time.sleep(0.5)
 
-		dump_count = 0
+def get_company_id(company):
+	company_encoded = urllib.parse.quote(company)
+	api1 = f"https://www.linkedin.com/voyager/api/voyagerOrganizationDashCompanies?decorationId=com.linkedin.voyager.dash.deco.organization.MiniCompany-10&q=universalName&universalName={company_encoded}"
+	r = requests.get(api1, headers=headers, cookies=cookies_dict)
+	response1 = r.json()
+	company_id = response1["elements"][0]["entityUrn"].split(":")[-1]
+	return company_id
 
-		def clean_data(data):
-			emoj = re.compile("["
-						u"\U0001F600-\U0001F64F"  # emoticons
-						u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-						u"\U0001F680-\U0001F6FF"  # transport & map symbols
-						u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-						u"\U00002500-\U00002BEF"  # chinese char
-						u"\U00002702-\U000027B0"
-						u"\U00002702-\U000027B0"
-						u"\U000024C2-\U0001F251"
-						u"\U0001f926-\U0001f937"
-						u"\U00010000-\U0010ffff"
-						u"\u2640-\u2642" 
-						u"\u2600-\u2B55"
-						u"\u200d"
-						u"\u23cf"
-						u"\u23e9"
-						u"\u231a"
-						u"\ufe0f"  # dingbats
-						u"\u3030"
-													"]+", re.UNICODE)
-			
-			# remove emojis
-			cleaned = re.sub(emoj, '', data).strip()
-			# convert german umlauts before removing diacritics
-			cleaned = cleaned.replace('Ü','Ue').replace('Ä','Ae').replace('Ö', 'Oe').replace('ü', 'ue').replace('ä', 'ae').replace('ö', 'oe')
-			# convert semicolon to colon to prevent CSV breaking
-			cleaned = cleaned.replace(',', '')
-			cleaned = cleaned.replace(';', ',')
-			# remove diacritics
-			cleaned = unidecode.unidecode(cleaned)
-			return cleaned.strip()
+def get_employee_data(company_id, start, count=10):
+	api2 = f"https://www.linkedin.com/voyager/api/search/dash/clusters?decorationId=com.linkedin.voyager.dash.deco.search.SearchClusterCollection-165&origin=COMPANY_PAGE_CANNED_SEARCH&q=all&query=(flagshipSearchIntent:SEARCH_SRP,queryParameters:(currentCompany:List({company_id}),resultType:List(PEOPLE)),includeFiltersInResponse:false)&count={count}&start={start}"
+	r = requests.get(api2, headers=headers, cookies=cookies_dict)
+	response2 = r.json()
+	return response2
 
-		def progressbar(it, prefix="", size=60, out=sys.stdout): # Python3.3+
-				count = len(it)
-				def show(j):
-						x = int(size*j/count)
-						print("{}[{}{}] {}/{}".format(prefix, "#"*x, "."*(size-x), j, count),
-										end='\r', file=out, flush=True)
-				show(0)
-				for i, item in enumerate(it):
-						yield item
-						show(i+1)
-				print("\n", flush=True, file=out)
+def clean_data(data):
+	emoj = re.compile("["
+				u"\U0001F600-\U0001F64F"  # emoticons
+				u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+				u"\U0001F680-\U0001F6FF"  # transport & map symbols
+				u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+				u"\U00002500-\U00002BEF"  # chinese char
+				u"\U00002702-\U000027B0"
+				u"\U00002702-\U000027B0"
+				u"\U000024C2-\U0001F251"
+				u"\U0001f926-\U0001f937"
+				u"\U00010000-\U0010ffff"
+				u"\u2640-\u2642" 
+				u"\u2600-\u2B55"
+				u"\u200d"
+				u"\u23cf"
+				u"\u23e9"
+				u"\u231a"
+				u"\ufe0f"  # dingbats
+				u"\u3030"
+									"]+", re.UNICODE)
+	
+	cleaned = re.sub(emoj, '', data).strip()
+	cleaned = cleaned.replace('Ü','Ue').replace('Ä','Ae').replace('Ö', 'Oe').replace('ü', 'ue').replace('ä', 'ae').replace('ö', 'oe')
+	cleaned = cleaned.replace(',', '')
+	cleaned = cleaned.replace(';', ',')
+	cleaned = unidecode.unidecode(cleaned)
+	return cleaned.strip()
 
-		employee_dict = []
-		# paginate API results
-		for page in progressbar(range(required_pagings), "Progress: ", 40):
-			if args.jitter:
-				jitter_dict = [0.5, 1, 0.8, 0.3, 3, 1.5, 5]
-				jitter = random.randrange(0, len(jitter_dict) - 1)
-				time.sleep(jitter)
+def parse_employee_results(results):
+	employee_dict = []
 
-			api2 = "https://www.linkedin.com/voyager/api/search/dash/clusters?decorationId=com.linkedin.voyager.dash.deco.search.SearchClusterCollection-165&origin=COMPANY_PAGE_CANNED_SEARCH&q=all&query=(flagshipSearchIntent:SEARCH_SRP,queryParameters:(currentCompany:List(" + str(companyID) + "),resultType:List(PEOPLE)),includeFiltersInResponse:false)&count=" + str(paging_count)+ "&start=" + str(page*10)
-			# retrieve employee information from the api based on previously obtained company id
-			r2 = requests.get(api2, headers=headers, cookies=cookies_dict)
-			response2 = r2.json()
+	for employee in results:
+		try:
+			account_name = clean_data(employee["itemUnion"]['entityResult']["title"]["text"]).split(" ")
+			badwords = ['Prof.', 'Dr.', 'M.A.', ',', 'LL.M.']
+			for word in list(account_name):
+				if word in badwords:
+					account_name.remove(word)
 
-			try:
-				test = response2["elements"][0]["items"][0]['itemUnion']['entityResult']['title']['text']
-				results = response2["elements"][0]["items"]
-			except:
-				pass
+			if len(account_name) == 2:
+				firstname = account_name[0]
+				lastname = account_name[1]
+			else:
+				firstname = ' '.join(map(str, account_name[0:(len(account_name)-1)]))
+				lastname = account_name[-1]
+		except:
+			continue
 
-			try:
-				test = response2["elements"][1]["items"][0]['itemUnion']['entityResult']['title']['text']
-				results = response2["elements"][1]["items"]
-			except:
-				pass
+		try:
+			position = clean_data(employee["itemUnion"]['entityResult']["primarySubtitle"]["text"])
+		except:
+			position = "N/A"
+		
+		gender = "N/A"
 
-			try:
-				test = response2["elements"][2]["items"][0]['itemUnion']['entityResult']['title']['text']
-				results = response2["elements"][2]["items"]
-			except:
-				pass
+		try:
+			location = employee["itemUnion"]['entityResult']["secondarySubtitle"]["text"]
+		except:
+			location = "N/A"
 
-			for employee in results:
+		try:
+			profile_link = employee["itemUnion"]['entityResult']["navigationUrl"].split("?")[0]
+		except:
+			profile_link = "N/A"
 
-				try:
-					# get a user's full account name and remove some known abbreviations and salutations
-					account_name = clean_data(employee["itemUnion"]['entityResult']["title"]["text"]).split(" ")
-					badwords = ['Prof.', 'Dr.', 'M.A.', ',', 'LL.M.']
-					for word in list(account_name):
-						if word in badwords:
-							account_name.remove(word)
+		if args.include_private_profiles:
+			employee_dict.append({"firstname": firstname, "lastname": lastname, "position": position, "gender": gender, "location": location, "profile_link": profile_link})
+		else:
+			if (firstname != "LinkedIn" and lastname != "Member"):
+				employee_dict.append({"firstname": firstname, "lastname": lastname, "position": position, "gender": gender, "location": location, "profile_link": profile_link})
+	
+	return employee_dict
 
-					# if the account name consists of 2 strings, assume firstname and lastname
-					if len(account_name) == 2:
-						firstname = account_name[0]
-						lastname = account_name[1]
-					# otherwise it is some unknown format with saluation, or middle name or random abbreviations
-					else:
-						# combine everything up to last string into firstname
-						firstname = ' '.join(map(str,account_name[0:(len(account_name)-1)]))
-						# use the last string as lastname
-						lastname = account_name[-1]
-				except:
-					# if the account name is not avail; just skip and process next entry
-					continue
+def progressbar(it, prefix="", size=60, out=sys.stdout): # Python3.3+
+	count = len(it)
+	def show(j):
+		x = int(size * j / count)
+		if not args.quiet:
+			print("{}[{}{}] {}/{}".format(prefix, "#" * x, "." * (size - x), j, count), end='\r', file=out, flush=True)
+	show(0)
+	for i, item in enumerate(it):
+		yield item
+		show(i + 1)
+	
+	if not args.quiet:
+		print("\n", flush=True, file=out)
 
-				try:
-					position = clean_data(employee["itemUnion"]['entityResult']["primarySubtitle"]["text"])
-				except:
-					position = "N/A"
-				
-				# gender is not avail in json; just provide it to be aligned to xingdumper
-				gender = "N/A"
+def main():
+	if url.startswith('https://www.linkedin.com/company/'):
+		try:
+			before_keyword, keyword, after_keyword = url.partition('company/')
+			company = after_keyword.split('/')[0]
 
-				try:
-					location = employee["itemUnion"]['entityResult']["secondarySubtitle"]["text"]
-				except:
-					location = "N/A"
+			if not args.quiet:
+				print_logo()
 
-				try:
-					profile_link = employee["itemUnion"]['entityResult']["navigationUrl"].split("?")[0]
-				except:
-					profile_link = "N/A"
+				stop_event = threading.Event()
+				loading_thread = threading.Thread(target=show_loading_message, args=(stop_event,))
+				loading_thread.start()
 
-				if args.include_private_profiles:
-					employee_dict.append({"firstname":firstname, "lastname":lastname, "position":position, "gender":gender, "location":location, "profile_link":profile_link})
-					dump_count += 1
-				else:
-					if (firstname != "LinkedIn" and lastname != "Member"):
-						employee_dict.append({"firstname":firstname, "lastname":lastname, "position":position, "gender":gender, "location":location, "profile_link":profile_link})
-						dump_count += 1
+			company_id = get_company_id(company)
 
-		# remove duplicates from list in dict
-		l = employee_dict
-		seen = set()
-		new_l = []
-		for d in l:
-			t = tuple(sorted(d.items()))
-			if t not in seen:
-				seen.add(t)
-				new_l.append(d)
+			api2_response = get_employee_data(company_id, 0)
+			paging_total = api2_response["paging"]["total"]
+			required_pagings = -(-paging_total // 10)
 
-			# this is the new dict with unique employees
+			if not args.quiet and api2_response:
+				stop_event.set()
+				loading_thread.join()
+				print()
+				print()
+
+			if not args.quiet:
+				print("[i] Company Name: " + company)
+				print("[i] Company X-ID: " + company_id)
+				print("[i] LN Employees: " + str(paging_total) + " employees found")
+				print("[i] Dumping Date: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+				if mailformat:
+					print("[i] Email Format: " + mailformat)
+				print()
+
+			employee_dict = []
+
+			for page in progressbar(range(required_pagings), "Progress: ", 40):
+				if args.jitter:
+					jitter_dict = [0.5, 1, 0.8, 0.3, 3, 1.5, 5]
+					jitter = random.choice(jitter_dict)
+					time.sleep(jitter)
+
+				api2_response = get_employee_data(company_id, page * 10)
+				for i in range(3):
+					try:
+						test = api2_response["elements"][i]["items"][0]['itemUnion']['entityResult']['title']['text']
+						results = api2_response["elements"][i]["items"]
+						employee_dict.extend(parse_employee_results(results))
+					except:
+						pass
+
+			l = employee_dict
+			seen = set()
+			new_l = []
+			for d in l:
+				t = tuple(sorted(d.items()))
+				if t not in seen:
+					seen.add(t)
+					new_l.append(d)
 			employee_dict = new_l
 
-		if mailformat:
-			legende = "Firstname;Lastname;Email;Position;Gender;Location;Profile"
-		else:
-			legende = "Firstname;Lastname;Position;Gender;Location;Profile"
-
-		print(legende)
-
-		# dump all crawled employees
-		for person in employee_dict:
 			if mailformat:
-				print(person["firstname"]+";"+person["lastname"]+";"+mailformat.format(person["firstname"].replace(".","").lower().translate(special_char_map),person["lastname"].replace(".","").lower().translate(special_char_map))+";"+person["position"]+";"+person["gender"]+";"+person["location"]+";"+person["profile_link"])
+				legende = "Firstname;Lastname;Email;Position;Gender;Location;Profile"
 			else:
-				print(";".join(person.values()))
+				legende = "Firstname;Lastname;Position;Gender;Location;Profile"
 
-		if not args.quiet:
-			print()
-			print("[i] Successfully crawled " + str(len(employee_dict)) + " unique " + str(company) + " employee(s). Hurray ^_-")
+			print(legende)
 
-	except Exception as e:
-		print("[!] Exception. Either API has changed and this script is broken or authentication failed.")
-		print("    > Set 'li_at' variable permanently in script or use the '--cookie' CLI flag!")
-		print("[debug] " + str(e))
-else:
-	print()
-	print("[!] Invalid URL provided.")
-	print("[i] Example URL: 'https://www.linkedin.com/company/apple'")
+			for person in employee_dict:
+				if mailformat:
+					print(person["firstname"]+";"+person["lastname"]+";"+mailformat.format(person["firstname"].replace(".","").lower().translate(special_char_map),person["lastname"].replace(".","").lower().translate(special_char_map))+";"+person["position"]+";"+person["gender"]+";"+person["location"]+";"+person["profile_link"])
+				else:
+					print(";".join(person.values()))
 
+			if not args.quiet:
+				print()
+				print("[i] Successfully crawled " + str(len(employee_dict)) + " unique " + str(company) + " employee(s). Hurray ^_-")
+
+		except Exception as e:
+			print("[!] Exception. Either API has changed and this script is broken or authentication failed.")
+			print("    > Set 'li_at' variable permanently in script or use the '--cookie' CLI flag!")
+			print("[debug] " + str(e))
+	else:
+		print()
+		print("[!] Invalid URL provided.")
+		print("[i] Example URL: 'https://www.linkedin.com/company/apple'")
+
+if __name__ == "__main__":
+	main()
+	
